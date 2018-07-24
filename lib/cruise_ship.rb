2335ccf3f9426a44d1e5dd210f46b30cfaff9a5b
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-24 04:55:05 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,9 @@
 # }
 
 def select_winner(passengers)
-   passengers["suite_a"]
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name 
+    end
 end
+winner
